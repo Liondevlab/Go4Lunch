8562fd477f55594cbb18eb5f4e41de63ee013f42
Liondevlab <julien.guerard@outlook.fr>
2021-12-23 10:07:12 +0100

    implementing details view with rating Stars and buttons mechanism

--- app/src/main/java/com/liondevlab/go4lunch/view/activity/RestaurantDetailsActivity.java
+++ app/src/main/java/com/liondevlab/go4lunch/view/activity/RestaurantDetailsActivity.java
@@ -15,10 +15,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.liondevlab.go4lunch.R;
 import com.liondevlab.go4lunch.model.Restaurant;
 import com.liondevlab.go4lunch.model.User;
-import com.liondevlab.go4lunch.viewmodel.MainViewModel;
 import com.liondevlab.go4lunch.viewmodel.RestaurantDetailsViewModel;
-import com.liondevlab.go4lunch.viewmodel.RestaurantMapViewModel;
-import com.liondevlab.go4lunch.viewmodel.SignInViewModel;
 
 public class RestaurantDetailsActivity extends AppCompatActivity {
 
@@ -30,7 +27,11 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
 
 	ImageButton mBackButton;
 	ImageView mRestaurantPicture;
-	ImageView mRating;
+	ImageView mRating1;
+	ImageView mRating2;
+	ImageView mRating3;
+	ImageView mRating4;
+	ImageView mRating5;
 	TextView mRestaurantName;
 	TextView mAddress;
 	FloatingActionButton mChoiceButton;
@@ -57,7 +58,11 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
 	private void setupView() {
 		mBackButton = findViewById(R.id.back_button);
 		mRestaurantPicture = findViewById(R.id.restaurant_picture);
-		mRating = findViewById(R.id.rating_image);
+		mRating1 = findViewById(R.id.rating_star_1);
+		mRating2 = findViewById(R.id.rating_star_2);
+		mRating3 = findViewById(R.id.rating_star_3);
+		mRating4 = findViewById(R.id.rating_star_4);
+		mRating5 = findViewById(R.id.rating_star_5);
 		mRestaurantName = findViewById(R.id.restaurant_name);
 		mAddress = findViewById(R.id.restaurant_type_and_location);
 		mChoiceButton = findViewById(R.id.choice_button);
@@ -85,6 +90,89 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
 		mAddress.setText(restaurant.getAddress());
 
 		mBackButton.setOnClickListener(v -> finish());
+
+		setRatingStars(restaurant);
+		setChoiceButton(restaurant);
+		setCallButton(restaurant);
+		setLikeButton(restaurant);
+		setWebsiteButton(restaurant);
+		setRecyclerView(restaurant);
+	}
+
+	private void setRatingStars(Restaurant restaurant) {
+		double rating = restaurant.getRating();
+		if (rating >= 0 && rating < 0.5) {
+			mRating1.setImageResource(R.drawable.ic_baseline_rating_star_off_24);
+		} else {
+			mRating1.setImageResource(R.drawable.ic_baseline_rating_star_on_24);
+		}
+		if (rating >= 0 && rating < 1.5) {
+			mRating2.setImageResource(R.drawable.ic_baseline_rating_star_off_24);
+		} else {
+			mRating2.setImageResource(R.drawable.ic_baseline_rating_star_on_24);
+		}
+		if (rating >= 0 && rating < 2.5) {
+			mRating3.setImageResource(R.drawable.ic_baseline_rating_star_off_24);
+		} else {
+			mRating3.setImageResource(R.drawable.ic_baseline_rating_star_on_24);
+		}
+		if (rating >= 0 && rating < 3.5) {
+			mRating4.setImageResource(R.drawable.ic_baseline_rating_star_off_24);
+		} else {
+			mRating4.setImageResource(R.drawable.ic_baseline_rating_star_on_24);
+		}
+		if (rating >= 0 && rating < 4.5) {
+			mRating5.setImageResource(R.drawable.ic_baseline_rating_star_off_24);
+		} else {
+			mRating5.setImageResource(R.drawable.ic_baseline_rating_star_on_24);
+		}
+	}
+
+	private void setChoiceButton(Restaurant restaurant) {
+		//TODO setup condition with ChosenRestaurant Collection
+
+		if (mIsChosen) {
+			mChoiceButton.setImageResource(R.drawable.ic_baseline_check_circle_24);
+		} else {
+			mChoiceButton.setImageResource(R.drawable.ic_baseline_check_circle_outline_24);
+		}
+
+		mChoiceButton.setOnClickListener(v -> {
+			if (mIsChosen) {
+				//TODO mRestaurantDetailsViewModel.deleteChoice(restaurant)
+			} else {
+				//TODO mRestaurantDetailsViewModel.createChoice(restaurant)
+			}
+		});
+
+	}
+
+	private void setCallButton(Restaurant restaurant) {
+
+	}
+
+	private void setLikeButton(Restaurant restaurant) {
+		//TODO setup condition with Favorite collection
+
+		if (mIsFavorite) {
+			mLikeButton.setImageResource(R.drawable.ic_baseline_like_star_on_24);
+		} else {
+			mLikeButton.setImageResource(R.drawable.ic_baseline_like_star_off_24);
+		}
+
+		if (mIsFavorite) {
+			//TODO mRestaurantDetailsViewModel.deleteFavorite(restaurant)
+		} else {
+			//TODO mRestaurantDetailsViewModel.createFavorite(restaurant)
+		}
+
+	}
+
+	private void setWebsiteButton(Restaurant restaurant) {
+
+	}
+
+	private void setRecyclerView(Restaurant restaurant) {
 
 	}
 
